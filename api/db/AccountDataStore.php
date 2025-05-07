785708eb1855a2bca9f027db58e2eed45c9d7a72
Gadiel Rangel <github@gadiel.dev>
2025-05-07 15:50:39 -0300

    refactor: add types, remove annotations from data store

--- api/db/AccountDataStore.php
+++ api/db/AccountDataStore.php
@@ -3,30 +3,19 @@
 use App\models\Account;
 
 /**
- * global state class (state is key/value)
+ * global state class
  * veeery simple approach for storing API state in memory
  */
 class AccountDataStore
 {
-    /** @var array */
     private static $accounts = [];
 
-    /**
-     * Reset all state
-     * @return bool
-     */
     public static function reset(): bool
     {
         self::$accounts = [];
         return true;
     }
 
-    /**
-     * Store an account
-     * @param string $id Account ID
-     * @param Account $account account
-     * @return int
-     */
     public static function createAccount(string $id, Account $account): ?int
     {
         if (!isset($id)) {
@@ -38,11 +27,6 @@ class AccountDataStore
         return $id;
     }
 
-    /**
-     * Get an account by ID
-     * @param string $id Account ID
-     * @return int|null Balance if account exists, null otherwise
-     */
     public static function getAccountById(?int $id): ?int
     {
         if (is_null($id)) {
